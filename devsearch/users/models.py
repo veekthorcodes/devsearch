eb36f1e1a5c98eb50b9a1c185f24a270c52a1f37
veekthorcodes <amadi490@yahoo.com>
2021-11-17 16:49:16 +0000

    added themes for user profiles and delivered contents from the database to the frontend

--- devsearch/users/models.py
+++ devsearch/users/models.py
@@ -3,12 +3,16 @@ from django.contrib.auth.models import User
 import uuid
 # Create your models here.
 
+
 class Profile(models.Model):
-    user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
+    user = models.OneToOneField(
+        User, null=True, blank=True, on_delete=models.CASCADE)
     name = models.CharField(max_length=200, null=True, blank=True)
     username = models.CharField(max_length=200, null=True, blank=True)
     email = models.EmailField(max_length=500, null=True, blank=True)
-    profile_image = models.ImageField(upload_to='profiles/', default='profiles/user-default')
+    profile_image = models.ImageField(
+        upload_to='profiles/', default='profiles/user-default.png')
+    location = models.CharField(max_length=200, null=True, blank=True)
     short_intro = models.CharField(max_length=200, null=True, blank=True)
     bio = models.TextField(null=True, blank=True)
     social_github = models.CharField(max_length=200, null=True, blank=True)
@@ -17,7 +21,22 @@ class Profile(models.Model):
     social_youtube = models.CharField(max_length=200, null=True, blank=True)
     social_website = models.CharField(max_length=200, null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
-    id = models.UUIDField(default=uuid.uuid4, primary_key=True, unique=True, editable=False)
+    id = models.UUIDField(default=uuid.uuid4,
+                          primary_key=True, unique=True, editable=False)
 
     def __str__(self):
         return str(self.user.username)
+
+
+class Skill(models.Model):
+    owner = models.ForeignKey(
+        Profile, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=200, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4,
+                          primary_key=True, unique=True, editable=False)
+
+
+    def __str__(self):
+        return str(self.name)
