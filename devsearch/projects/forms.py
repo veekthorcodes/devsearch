245d2ad2b51f557cf4de1739f35fadd090584dc2
veekthorcodes <amadi490@yahoo.com>
2021-11-17 13:35:48 +0000

    installed most of the theme

--- devsearch/projects/forms.py
+++ devsearch/projects/forms.py
@@ -1,8 +1,21 @@
 from django.forms import ModelForm
+from django import forms
 from .models import Project
+
 
 class ProjectForm(ModelForm):
     class Meta:
         model = Project
         fields = '__all__'
         exclude = ['vote_total', 'vote_ratio']
+        widgets = {
+            'tags': forms.CheckboxSelectMultiple()
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(ProjectForm, self).__init__(*args, **kwargs)
+        for name, field in self.fields.items():
+            field.widget.attrs.update({
+                'class': 'input',
+                'placeholder': f'Enter {field.label}'
+            })
