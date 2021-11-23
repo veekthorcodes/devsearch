20eeb8da20f8313d8b41cf4530576a836596b454
veekthorcodes <amadi490@yahoo.com>
2021-11-23 08:24:23 -0800

    added email sending functionality

--- devsearch/users/signals.py
+++ devsearch/users/signals.py
@@ -2,6 +2,8 @@ from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from .models import Profile
 from django.contrib.auth.models import User
+from django.core.mail import message, send_mail
+from django.conf import settings
 
 
 def profileUpdated(sender, instance, created, **kwargs):
@@ -12,6 +14,17 @@ def profileUpdated(sender, instance, created, **kwargs):
             username=user.username,
             email=user.email,
             name=user.first_name
+        )
+
+        subject = 'Welcome to DevSearch'
+        message = 'Thank you for signing up with DevSearch!'
+
+        send_mail(
+            subject,
+            message,
+            settings.EMAIL_HOST_USER,
+            [profile.email,],
+            fail_silently=False
         )
 
 
@@ -27,9 +40,11 @@ def profileEdited(sender, instance, created, **kwargs):
 
 
 def deleteUser(sender, instance, **kwargs):
-    user = instance.user
-    user.delete()
-
+    try:
+        user = instance.user
+        user.delete()
+    except:
+        pass
 
 
 post_save.connect(profileUpdated, sender=User)
